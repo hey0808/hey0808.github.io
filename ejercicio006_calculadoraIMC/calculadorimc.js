ef157a4048ee439e780235990e38ad6b12e3bb58
hey0808 <damar.dr8@gmail.com>
2024-07-04 17:08:07 +0200

    gggg

---
+++ ejercicio006_calculadoraIMC/calculadorimc.js
@@ -0,0 +1,10 @@
+// creamos una funcion 
+//ponemos variables lo que necesite para que funciona la funcion 
+
+function calcularImc(peso,altura){
+
+    let imc= peso/((altura/100)**2)
+
+    return imc
+    
+}
